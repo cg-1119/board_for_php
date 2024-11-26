5255846d68876e196d8bd039957a596664a23a1e
cg-1119 <148218775+cg-1119@users.noreply.github.com>
2024-11-26 10:14:10 +0900

    chore: table 오타 수정

--- board/tables.sql
+++ board/tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE posts (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
-     created_at VARCHAR(255) NOT NULL,
+    created_at VARCHAR(255) NOT NULL,
     has_attachment TINYINT(1) DEFAULT 0,
     username VARCHAR(100) NOT NULL,
     userpassword VARCHAR(255) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE comments (
     content TEXT NOT NULL,
     username VARCHAR(45) NOT NULL,
     userpassword VARCHAR(255) NOT NULL,
-    reated_at VARCHAR(255) NOT NULL,
+    created_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
 )DEFAULT CHARSET=utf8;
